f1f441d4fe6a71415549e4c7b3445dfcde42b449
Santana <santana.tux@gmail.com>
2025-10-16 23:37:30 -0300

    Configuração do banco PostgreSQL no Railway

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -104,7 +104,13 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, conn_health_checks=True,)
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback local
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
