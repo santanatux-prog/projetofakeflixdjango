bc255143825b7b1b514a0970f588b9823adaa015
Santana <santana.tux@gmail.com>
2025-10-16 22:46:20 -0300

    update DATABASE_URL

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -97,7 +97,8 @@ DATABASES = {
 import dj_database_url
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+#DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql://postgres:AQdzWVqsEuSWqUyaywquUVAlcBRwNqXk@postgres.railway.internal:5432/railway'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
